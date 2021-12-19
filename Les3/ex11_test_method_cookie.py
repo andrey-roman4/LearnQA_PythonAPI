26cc839ed62bc154d1b8f9e33ead0ca65d91752b
Andrey Romanenko <aromanenko@artec3d.com>
2021-12-19 16:47:17 +0100

    Homework for Ex11: Test cookie method

---
+++ Les3/ex11_test_method_cookie.py
@@ -0,0 +1,10 @@
+import requests
+
+def test_cookies_method():
+    coo_value = 'hw_value'
+    response = requests.request('GET', 'https://playground.learnqa.ru/api/homework_cookie')
+    assert response.status_code == 200, "Response status is not 200"
+    assert dict(response.cookies), "There is no cookies in response"
+    cookie_key_list = response.cookies.keys()
+    cookie_value = response.cookies.get(cookie_key_list[0])
+    assert cookie_value == coo_value, f"Cookie value is not '{coo_value}'"
